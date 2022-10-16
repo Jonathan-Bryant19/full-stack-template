4fcd628e9c4dc732d04aed064a3a3d26f3f943dd
Jonathan Bryant <spectrum19tech@gmail.com>
2022-10-15 19:41:15 -0700

    Add Login and Signup components and complete simple styling

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,9 +1,39 @@
 import React from 'react'
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        <div className='container'>
+            <div className='row mt-3'>
+                <h1 className='col' align='center'>Sign Up</h1>
+            </div>
+            <div className='row w-50 login' >
+                <form className='col'>
+                    <div className="my-3">
+                        <label for="name" className="form-label">Name</label>
+                        <input type="text" className="form-control" id="name" />
+                    </div>
+                    <div className='mb-3'>
+                        <label for="email" className="form-label">Email</label>
+                        <input type="email" className="form-control" id="email" aria-describedby="emailHelp" />
+                        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                    </div>
+                    <div className="mb-3">
+                        <label for="password" className="form-label">Password</label>
+                        <input type="password" className="form-control" id="password" />
+                    </div>
+                    <div className="mb-3">
+                        <label for="confirmPassword" className="form-label">Confirm Password</label>
+                        <input type="password" className="form-control" id="confirmPassword" />
+                    </div>
+                    <div className="mb-3 form-check">
+                        <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                        <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    )
 }
 
 export default Signup
